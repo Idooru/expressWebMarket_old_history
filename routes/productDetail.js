ede18689fe26d2cecb5f24f6e5b9a070c1140f63
shere1765 <shere1765@gmail.com>
2022-02-08 18:47:38 +0900

    0.2.0.8 use SQL database

--- routes/productDetail.js
+++ routes/productDetail.js
@@ -6,53 +6,14 @@ const router = express.Router();
 router.get("/:id/info", async (req, res, next) => {
     try {
         let productInfo = await Product.findAll({});
-        // let dataBox = [];
+        const params = req.params.id;
 
-        // for (let key in productInfo) {
-        //     if (key === "0") continue;
-        //     dataBox.push(productInfo[key].dataValues);
-        // }
-
-        switch (req.params.id) {
-            case "1":
-                productInfo = productInfo[1];
-                break;
-            case "2":
-                productInfo = productInfo[2];
-                break;
-            case "3":
-                productInfo = productInfo[3];
-                break;
-            case "4":
-                productInfo = productInfo[4];
-                break;
-            case "5":
-                productInfo = productInfo[5];
-                break;
-            case "6":
-                productInfo = productInfo[6];
-                break;
-            case "7":
-                productInfo = productInfo[7];
-                break;
-            case "8":
-                productInfo = productInfo[8];
-                break;
-            case "9":
-                productInfo = productInfo[9];
-                break;
-            case "10":
-                productInfo = productInfo[10];
-                break;
-            case "11":
-                productInfo = productInfo[11];
-                break;
-
-            default:
-                res.locals.message = "No more product on that url";
-                res.render("noProduct");
-                return 0;
+        if (productInfo[params] === undefined) {
+            res.render("noProduct");
+            return;
         }
+
+        productInfo = productInfo[params];
 
         res.locals.id = req.params.id;
         res.locals.productName = productInfo.name;
