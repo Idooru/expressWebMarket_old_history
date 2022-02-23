48af7f1071e166bd72dc7ae5e54c1c5ec548f4aa
root <root@Idooru.localdomain>
2022-02-23 21:13:04 +0900

    0.2.0.23

--- app.js
+++ app.js
@@ -122,7 +122,9 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(app.get("port"), () => {
-    console.log(`API server is running at http://localhost:${app.get("port")}`);
+    console.log(
+        `### API server is running at http://localhost:${app.get("port")} ###`
+    );
 });
 
 // test server
